92a8afd90d84e93b2369e8ccc9259f26f3035cf7
tezerumut <tezerumut@gmail.com>
2026-01-31 00:36:45 +0300

    Create words.js

---
+++ src/words.js
@@ -0,0 +1,21 @@
+// src/words.js
+export const OXFORD_3000 = {
+  "A1-A2": [
+    { eng: "Ability", tr: "Yetenek", ex: "He has the ability to swim." },
+    { eng: "Abandon", tr: "Terk etmek", ex: "Don't abandon your hope." },
+    { eng: "Absolute", tr: "Kesin, tam", ex: "It's the absolute truth." },
+    { eng: "Academic", tr: "Akademik", ex: "He has an academic career." },
+    { eng: "Accompany", tr: "Eşlik etmek", ex: "May I accompany you?" },
+    { eng: "Account", tr: "Hesap", ex: "I need to open a bank account." },
+    { eng: "Accurate", tr: "Doğru, kesin", ex: "This map is very accurate." },
+    { eng: "Accuse", tr: "Suçlamak", ex: "Don't accuse me of lying." },
+    { eng: "Achieve", tr: "Başarmak", ex: "You can achieve anything." },
+    { eng: "Acknowledge", tr: "Kabul etmek, tanımak", ex: "He acknowledged his mistake." }
+    // Buraya 3000 kelimeye kadar ekleme yapabiliriz.
+  ],
+  "B1-B2": [
+    { eng: "Beneficial", tr: "Faydalı", ex: "Exercise is beneficial for health." },
+    { eng: "Capacity", tr: "Kapasite", ex: "The hall has a large capacity." },
+    { eng: "Challenge", tr: "Zorluk, mücadele", ex: "It was a big challenge for me." }
+  ]
+};
